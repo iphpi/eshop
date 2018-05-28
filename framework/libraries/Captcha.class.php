752133bd3b5806c588656a77258940cb875d01dc
iphpi <q_dl@sina.com>
2018-05-28 22:47:05 +0800

    Ubuntu严格区分路径
    14行字体加了./相对路径
    解决方法：
    虽然上面代码指明的是字体文件在当前目录下，但是Ubuntn的apache服务器中还是要给字体文件指定明确的路径
    $this->font = './elephant.ttf';    //相对路径
    //或者
    $this->font = '/var/www/html/project/elephant.ttf';    //绝对路径
    这样验证码就可以正常显示了
    从上面的函数用法来看就是打不开字体了，也就是一个非常简单路径问题了。

--- framework/libraries/Captcha.class.php
+++ framework/libraries/Captcha.class.php
@@ -11,7 +11,7 @@ class Captcha{
     private $fontcolor;                //指定字体颜色
 
     //构造方法初始化
-    public function __construct($codelen=4,$width=150,$height=40,$fontsize = 25,$font="elephant.ttf") {
+    public function __construct($codelen=4,$width=150,$height=40,$fontsize = 25,$font="./elephant.ttf") {
 		$this->codelen = $codelen;
 		$this->width = $width;
 		$this->height = $height;
